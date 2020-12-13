3726e4024bce12cac03c736601fb45ebcd12ae30
wolf-yuan-6115 <h1030035@gmail.com>
2020-12-13 15:50:01 +0800

    0

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ if (urlParams.has("server")) {
         document.getElementById("songtitle").innerHTML = "無法取得伺服器的播放狀態..."
     }
     setInterval(function() {
-        request.open("GET", "https://blackcatbot.herokuapp.com/api/playing", false);
+        request.open("GET", "https://blackcatbot.herokuapp.com/api/playing?server=" + urlParams.get("server"), false);
         request.send();
     })
 } else {
     document.getElementById("songtitle").innerHTML = "你沒有指定要查詢哪個伺服器的播放狀態!";
     document.getElementById("loader").style.display = "none";
     document.getElementById("thumbnail").style.display = "none";
+    document.getElementById("container").style.backgroundColor = "rgba(0,0,0,0)";
+    document.getElementById("songtitle").style.color = "#ffffff"
 }
